db320b73bac2b40d5f7655e17dac1199dd6eaede
Tim Stahl <timothystahl@gmail.com>
2019-12-29 20:40:21 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,12 @@
 ﻿using System;
 
-namespace ConsoleUI
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("What is your age: ");
-            string ageText = Console.ReadLine();
-
-            //ageText += 10;
-            //Console.WriteLine($"You are going to be {ageText} years old in 10 years");
-
-            //int age = int.Parse(ageText);
-            //age += 10;
-            
-            bool isAgeValid = int.TryParse(ageText, out int age);
-            if (isAgeValid)
-            {
-                age += 10;
-                Console.WriteLine($"You are going to be {age} years old in 10 years"); 
-            }
-            else
-            {
-                Console.WriteLine("That is not a valid age.");
-            }
-
-            Console.ReadLine();
+            Console.WriteLine("Hello World!");
         }
     }
 }
